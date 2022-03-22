40c87ccc5a2c0875596466308e00b9b5ce5c2c00
pranavs28 <pranav.senthilvel@gmail.com>
2022-03-22 17:00:43 -0400

    Created addFeedback functionality

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -22,7 +22,7 @@ function FeedbackList({feedback, handleDelete}) {
 FeedbackList.propTypes = {
     feedback: PropTypes.arrayOf(
         PropTypes.shape({
-            id: PropTypes.number.isRequired,
+            id: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
             text: PropTypes.string.isRequired,
             rating: PropTypes.number.isRequired
         })
