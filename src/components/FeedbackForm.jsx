eaecf7c3fc3fe3dc5cc25dd67cf4b92f035de19d
pranavs28 <pranav.senthilvel@gmail.com>
2022-03-22 16:45:51 -0400

    Added rating system

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -1,6 +1,7 @@
 import Card from "./shared/Card"
 import { useState } from "react"
 import Button from "./shared/Button"
+import RatingSelect from "./RatingSelect"
 
 
 function FeedbackForm() {
@@ -8,6 +9,7 @@ function FeedbackForm() {
     const [text, setText] = useState('')
     const [btnDisabled, setBtnDisabled] = useState(true)
     const [message, setMessage] = useState('')
+    const [rating, setRating] = useState(10)
 
     const handleTextChange = (e) => {
         if(e.target.value === ''){
@@ -29,7 +31,7 @@ function FeedbackForm() {
     <Card>
         <form>
             <h2>How would you rate your service with us?</h2>
-            {/* TODO rating select component */}
+            <RatingSelect select={(rating) => console.log(rating)}/>
             <div className="input-group">
                 <input onChange = {handleTextChange} 
                 type="text" 
