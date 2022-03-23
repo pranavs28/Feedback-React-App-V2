44e32d8ff880e726b97856471c86fb4973400020
pranavs28 <pranav.senthilvel@gmail.com>
2022-03-23 00:44:04 -0400

    Add load functionality and JSON-Server connectivity

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -1,4 +1,4 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
 import {v4 as uuidv4} from 'uuid' 
 
 const FeedbackContext = createContext()
@@ -6,28 +6,25 @@ const FeedbackContext = createContext()
 
 
 export const FeedbackProvider = ({children}) => {
-    const [feedback, setFeedback] = useState([
-        {
-            id: 1,
-            text: 'This is feedback item 1',
-            rating: 10
-        },
-        {
-            id: 2,
-            text: 'This is feedback item 2',
-            rating: 9
-        },
-        {
-            id: 3,
-            text: 'This is feedback item 3',
-            rating: 7
-        }
-    ])
+    const [isLoading, setIsLoading] = useState(true)
+    const [feedback, setFeedback] = useState([])
 
     const [feedbackEdit, setFeedbackEdit] = useState({
         item: {},
         edit: false
     })
+
+    useEffect( () => {
+        fetchFeedback()
+    }, [])
+
+    // Fetch feedback data
+    const fetchFeedback = async () => {
+        const response = await fetch('http://localhost:5000/feedback?_sort=id_order=desc')
+        const data = await response.json()
+        setFeedback(data)
+        setIsLoading(false)
+    }
 
     // Delete Feedback
     const deleteFeedback = (id) => {
@@ -53,7 +50,7 @@ export const FeedbackProvider = ({children}) => {
         setFeedback(feedback.map((item)=> item.id === id ? {...item,...updateItem} : item))
     }
     
-    return <FeedbackContext.Provider value={{ feedback, deleteFeedback, addFeedback, editFeedback, feedbackEdit, updateFeedback }}>
+    return <FeedbackContext.Provider value={{ feedback, deleteFeedback, addFeedback, editFeedback, feedbackEdit, updateFeedback, isLoading }}>
         {children}
     </FeedbackContext.Provider>
 }
